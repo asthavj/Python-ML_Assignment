3560d89f3f0ccb7caccd07867d997c1191ef25e5
AsthaVijay <asthavijay1903@gmail.com>
2024-06-14 10:01:27 +0530

    First ques uploaded

---
+++ Ques1.py
@@ -0,0 +1,4 @@
+#1 Write a program that takes two numbers as input and prints their sum.
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("sum of two numbers :",a+b)
